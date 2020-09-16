ba2217f19cc9b0ddb80e7cf5ddb5e018e52e44ef
robertelias <robert.elias@live.com>
2020-09-16 11:29:32 -0700

    updated solutions

--- Easy/TwoNumSum.py
+++ Easy/TwoNumSum.py
@@ -1,3 +1,5 @@
+array = [3,5,-4,8,11,-1,6]
+targetSum = 10
 def twoNumberSum(array, targetSum):
     #for loop to iterate the array
     for i in range(len(array) - 1):
@@ -7,6 +9,25 @@ def twoNumberSum(array, targetSum):
             if firstNum + secondNum == targetSum:
                 return [firstNum, secondNum]
     return[]
-    print(twoNumberSum(3,2))
+#    print(twoNumberSum(firstNum, secondNum))
 
-def twoNumSum() 
+# Second Solution
+def twoNumberSum(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []
+
+# Third Solution
+def TwoNumSum(array, targetSum):
+    for i in range(len(array)-1):
+        firstNum = array[i]
+        for j in range( i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
