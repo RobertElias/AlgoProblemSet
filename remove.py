5e5afca7e0bb1318edf0b6302a202d832fe5ee9d
robertelias <robert.elias@live.com>
2020-09-24 11:28:12 -0700

    updates

--- remove.py
+++ remove.py
@@ -3,43 +3,31 @@ class Node:
 	def __init__(self, value=None, next=None):
 		self.value = value
 		self.next = next
-
-
 # Helper function to print given linked list
-def printList(head):
-
-	ptr = head
+def printList(node):
+	ptr = node
 	while ptr:
 		print(ptr.value, end=" -> ")
 		ptr = ptr.next
 	print("None")
-
-
-# Function to remove duplicates from a sorted list
-def removeDuplicates(head):
-
+# ******Function to remove duplicates from a sorted list********
+def removeDuplicates(node):
 	previous = None
-	current = head
-
+	current = node
 	# take an empty set to store linked list nodes for future reference
 	s = set()
-
 	# do till linked list is not empty
 	while current:
-
 		# if current node is seen before, ignore it
 		if current.value in s:
 			previous.next = current.next
-
 		# insert current node into the set and proceed to next node
 		else:
 			s.add(current.value)
 			previous = current
-
 		current = previous.next
-
-	return head
-
+	return node
+#*****************************************************************
 
 if __name__ == '__main__':
 
